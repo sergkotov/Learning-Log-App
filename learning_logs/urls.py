e46cd1011b6497c712f5cee78bcc5ffb3d6f53c3
Sergei <sy-kotov.gmail.com>
2020-02-21 18:55:58 +0300

    functions for adding topics, entries and editing entries appended

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
 	path('topics/', views.topics, name='topics'),
 	#page with information for the theme
 	path('topics/<int:topic_id>', views.topic, name='topic'),
+	#page for adding a new topic
+	path('new_topic/', views.new_topic, name='new_topic'),
+	#page for adding a new entry
+	path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+	#page for editing the entry
+	path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 ]
