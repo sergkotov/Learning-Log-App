a8e64ca9b53071a4651aec1c83055b90cd2c4d4e
Sergei <sy-kotov.gmail.com>
2020-02-25 17:59:42 +0300

    Added custom 404 and 500 error pages

--- lerning_log/settings.py
+++ lerning_log/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'lerning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'lerning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
