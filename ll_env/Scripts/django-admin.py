66b009ec281390cd0df5f61784b7b65aa735ebdc
Sergei <sy-kotov.gmail.com>
2020-02-26 16:22:18 +0300

    extra folders were deleted

--- ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sykot\onedrive\desktop\cod\django\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
